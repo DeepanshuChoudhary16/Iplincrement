9aa45000edf6d26e4c890a6090e5b297d0cd462e
DeepanshuChoudhary16 <dummy@gmail.com>
2026-04-16 06:25:37 +0000

    Day-8

--- server/src/main/java/com/edutech/progressive/controller/MatchController.java
+++ server/src/main/java/com/edutech/progressive/controller/MatchController.java
@@ -17,70 +17,125 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
 import java.util.List;
-@RestController @RequestMapping("/match")
+@RestController
+
+@RequestMapping("/match")
+
 public class MatchController {
 
     @Autowired
+
     MatchServiceImplJpa matchServiceImplJpa;
+
     @GetMapping
-    public ResponseEntity<List<Match>> getAllMatches()  {
-        // try {
-        //     return new ResponseEntity<>(matchServiceImplJpa.getAllMatches(),HttpStatus.OK);
-        // } catch (Exception e) {
-        //     return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        // }
-        return null;
-    }
-    
-    @GetMapping("/{matchId}")
-    public ResponseEntity<Match> getMatchById(@PathVariable int matchId)  {
-    //    try {
-    //      return new ResponseEntity<>(matchServiceImplJpa.getMatchById(matchId),HttpStatus.OK);
-    //    } catch (Exception e) {
-    //     return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    //     }
-    //    }
-    return null;
+
+    public ResponseEntity<List<Match>> getAllMatches() {
+
+        try {
+
+            List<Match> matchList = matchServiceImplJpa.getAllMatches();
+
+            return new ResponseEntity<>(matchList, HttpStatus.OK);
+
+        } catch (SQLException e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
     }
 
-   
-    public ResponseEntity<Integer> addMatch(@RequestBody Match match)  {
-        // try {
-        //     return new ResponseEntity<>(matchServiceImplJpa.addMatch(match),HttpStatus.OK) ;
-        // } catch (Exception e) {
-        //     return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        // }
-        return null;
+    @GetMapping("/{matchId}")
+
+    public ResponseEntity<Match> getMatchById(@PathVariable int matchId) {
+
+        try {
+
+            Match match = matchServiceImplJpa.getMatchById(matchId);
+
+            return new ResponseEntity<>(match, HttpStatus.OK);
+
+        } catch (SQLException e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+    }
+
+    @PostMapping
+
+    public ResponseEntity<Integer> addMatch(@RequestBody Match match) {
+
+        try {
+
+            int matchId = matchServiceImplJpa.addMatch(match);
+
+            return new ResponseEntity<>(matchId, HttpStatus.CREATED);
+
+        } catch (SQLException e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
     }
 
     @PutMapping("/{matchId}")
-    public ResponseEntity<Void> updateMatch(@PathVariable int matchId, Match match)   {
-        // try {
-        //     matchServiceImplJpa.updateMatch(match);
-        //     return new ResponseEntity<>(HttpStatus.OK) ;
-        // } catch (Exception e) {
-        //     return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        // }
-        return null;
-    }
-    
-    @DeleteMapping("/{matchId}")
-    public ResponseEntity<Void> deleteMatch(@PathVariable int matchId)  {
-        // try {
-        //     matchServiceImplJpa.deleteMatch(matchId);
-        //     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        // } catch (Exception e) {
-        //     return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        // }
-        return null;
+
+    public ResponseEntity<Void> updateMatch(@PathVariable int matchId, @RequestBody Match match) {
+
+        try {
+
+            match.setMatchId(matchId);
+
+            matchServiceImplJpa.updateMatch(match);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        } catch (SQLException e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
     }
 
-    public ResponseEntity<List<Match>> getAllMatchesByStatus(String status) {
-        // try {
-        //     return new ResponseEntity<>(matchServiceImplJpa.getAllMatchesStatus(status),HttpStatus.OK);
-        // } catch (Exception e) {
-        //     return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        // }
-        return null;
+    @DeleteMapping("/{matchId}")
+
+    public ResponseEntity<Void> deleteMatch(@PathVariable int matchId) {
+
+        try {
+
+            matchServiceImplJpa.deleteMatch(matchId);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        } catch (SQLException e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
     }
+
+        @GetMapping("/status/{status}")
+
+    public ResponseEntity<?> getAllMatchesByStatus(@PathVariable String status) {
+
+        try {
+
+            List<Match> matchList = matchServiceImplJpa.getAllMatchesByStatus(status);
+
+            return new ResponseEntity<>(matchList, HttpStatus.OK);
+
+        }  catch (Exception e) {
+
+            return new ResponseEntity<>("An unexpected error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);  
+
+        }
+
+    }
+
 }
+ 
--- server/src/main/java/com/edutech/progressive/service/impl/MatchServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/MatchServiceImplJpa.java
@@ -14,52 +14,48 @@ import com.edutech.progressive.service.MatchService;
 @Service
 public class MatchServiceImplJpa implements MatchService {
 
-     @Autowired
-    MatchRepository matchRepository;
-    
+    private MatchRepository matchRepository;
+
+    // @Autowired
+    // private TicketBookingRepository ticketBookingRepository;
+
+    @Autowired
     public MatchServiceImplJpa(MatchRepository matchRepository) {
         this.matchRepository = matchRepository;
     }
-    List<Match> list = new ArrayList<>();
- 
 
-    public List<Match> getAllMatches() throws SQLException
-    { 
-        return matchRepository.findAll();  
+    @Override
+    public List<Match> getAllMatches() throws SQLException {
+        return matchRepository.findAll();
     }
-    public Integer addMatch(Match match)  throws SQLException
-    {
-        list.add(match);
+
+    @Override
+    public Match getMatchById(int matchId) throws SQLException {
+        return matchRepository.findByMatchId(matchId);
+    }
+
+    @Override
+    public Integer addMatch(Match match) throws SQLException {
+        return matchRepository.save(match).getMatchId();
+    }
+
+    @Override
+    public void updateMatch(Match match) throws SQLException {
         matchRepository.save(match);
-        return match.getMatchId();
     }
-    public Match getMatchById(int matchId)  throws SQLException
-    {
-        // return matchRepository.findByMatchId(matchId);
-        return null;
-        
-    }
-    public void updateMatch(Match match)  throws SQLException
-    {
-         Match match2 = matchRepository.findById(match.getMatchId()).get();
-         match2.setFirstTeamId(match.getFirstTeamId());
-         match2.setMatchDate(match.getMatchDate());
-         match2.setMatchId(match.getMatchId());
-         match2.setResult(match.getResult());
-         match2.setSecondTeamId(match.getSecondTeamId());
-         match2.setStatus(match.getStatus());
-         match2.setWinnerTeamId(match.getWinnerTeamId());
-    }
-    public void deleteMatch(int matchId)  throws SQLException
-    {
-         matchRepository.deleteById(matchId);
-    }
-    public List<Match> getAllMatchesStatus(String status)  throws SQLException
-    {
-        //   return matchRepository.findAllByStatus(status);
-        return null;
-    }
-    
-   
 
+    @Override
+    public void deleteMatch(int matchId) throws SQLException {
+        // ticketBookingRepository.deleteByMatchId(matchId);
+        matchRepository.deleteById(matchId);}
+
+    @Override
+    public List<Match> getAllMatchesByStatus(String status) throws SQLException {
+        List<Match> matchList = matchRepository.findAllByStatus(status);
+        if (matchList.isEmpty()) {
+            // throw new NoMatchesFoundException("No matches found with status = " + status);
+            return null;
+        }
+        return matchList;
+    }
 }

a5add012cb70f8da9775207db4812ff123d975ce
DeepanshuChoudhary16 <dummy@gmail.com>
2026-04-16 14:49:05 +0000

    Day-11

--- server/src/main/java/com/edutech/progressive/repository/TeamRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/TeamRepository.java
@@ -1,13 +1,17 @@
 package com.edutech.progressive.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edutech.progressive.entity.Team;
 
 public interface TeamRepository extends JpaRepository<Team,Integer> {
 
-    public Team findByTeamId(int teamId);
-    public void deleteByTeamId(int teamId);
+   Team findByTeamId(int teamId);
+
+    Team findByTeamName(String teamName);
+
     // public Integer findByTeamId(int teamId);
 
 }
